19b58731d8dda5ec546578f269d1226824bd0344
D. Puplauskas <puplauskas@hotmail.com>
2024-05-02 23:12:05 +0200

    parse cli args using clap library

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,7 @@ struct Cli {
 
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
