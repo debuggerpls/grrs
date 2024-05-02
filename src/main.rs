fb3342c82ba1df7eb8abc6eba6f78b242d782e66
D. Puplauskas <puplauskas@hotmail.com>
2024-05-02 23:34:54 +0200

    completed exercise for the reader with BufReader implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -13,11 +16,19 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let file = std::fs::File::open(args.path).expect("could not open file");
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    while let Ok(len) = reader.read_line(&mut line) {
+        if len == 0 {
+            break;
         }
+
+        if line.contains(&args.pattern) {
+            print!("{}", line);
+        }
+        
+        line.clear();
     }
 }
